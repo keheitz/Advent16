5df9ccb3357f90f373c63044333598f72ee302ec
KEH <keheitz@gmail.com>
2016-12-10 23:28:39 -0600

    day 6

---
+++ Advent16.Tests/Controllers/Day5ControllerTest.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Advent16;
+using Advent16.Controllers;
+
+
+namespace Advent16.Tests.Controllers
+{
+    [TestClass]
+    public class Day5ControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            Day2Controller controller = new Day2Controller();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        //Testing first line of example problem with both methods
+
+        [TestMethod]
+        public void GetPassword()
+        {
+            //Arrange
+            var controller = new Day5Controller();
+            Models.DoorInput input = new Models.DoorInput();
+            input.DoorID = "abc";
+            // Act
+            ActionResult result = controller.GetPassword(input);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(PartialViewResult));
+            PartialViewResult viewResult = (PartialViewResult)result;
+
+            Assert.IsInstanceOfType(viewResult.Model, typeof(Models.DoorPassword));
+            Models.DoorPassword model = (Models.DoorPassword)viewResult.Model;
+            Assert.AreEqual("18F47A30", model.Password);
+        }
+
+        [TestMethod]
+        public void GetSecondPassword()
+        {
+            var controller = new Day5Controller();
+            Models.DoorInput input = new Models.DoorInput();
+            input.DoorID = "abc";
+
+            // Act
+            ActionResult result = controller.GetSecondPassword(input);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(PartialViewResult));
+            PartialViewResult viewResult = (PartialViewResult)result;
+
+            Assert.IsInstanceOfType(viewResult.Model, typeof(Models.DoorPassword));
+            Models.DoorPassword model = (Models.DoorPassword)viewResult.Model;
+            Assert.AreEqual("05ACE8E3", model.Password);
+        }
+    }
+}
---
+++ Advent16/Controllers/Day6Controller.cs
@@ -0,0 +1,59 @@
+﻿using Advent16.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Advent16.Controllers
+{
+    public class Day6Controller : Controller
+    {
+        // GET: Day6
+        public ActionResult Index()
+        {
+            SantaComm comm = new SantaComm();
+            return View(comm);
+        }
+
+        [HttpPost]
+        public ActionResult GetCorrectedComm(SantaComm comm)
+        {
+            StringBuilder msg = new StringBuilder();
+            for (int i = 0; i < comm.RepetitionCode.First().Length; i++)
+            {
+                List<LetterCount> count = comm.RepetitionCode.Select(x => x[i]).GroupBy(c => c).Select(x => new LetterCount
+                {
+                    Letter = x.Key,
+                    Count = x.Count()
+                }).ToList();
+                LetterCount mostfrequent = count.OrderByDescending(x => x.Count).Take(1).First();
+                msg.Append(mostfrequent.Letter);
+            }
+            CorrectedMessage correctcomm = new CorrectedMessage();
+            correctcomm.Message = msg.ToString();
+            return PartialView("Message", correctcomm);
+        }
+
+        [HttpPost]
+        public ActionResult DecodeModifiedRepetition(SantaComm comm)
+        {
+            StringBuilder msg = new StringBuilder();
+            for (int i = 0; i < comm.RepetitionCode.First().Length; i++)
+            {
+                List<LetterCount> count = comm.RepetitionCode.Select(x => x[i]).GroupBy(c => c).Select(x => new LetterCount
+                {
+                    Letter = x.Key,
+                    Count = x.Count()
+                }).ToList();
+                LetterCount leastfrequent = count.OrderBy(x => x.Count).Take(1).First();
+                msg.Append(leastfrequent.Letter);
+            }
+            CorrectedMessage correctcomm = new CorrectedMessage();
+            correctcomm.Message = msg.ToString();
+            return PartialView("Message", correctcomm);
+        }
+
+    }
+}
